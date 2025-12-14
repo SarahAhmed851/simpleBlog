41a1206fbe6e585469066295394250f02a010423
Sarah Ahmed <sarahabumedianahmed22@gmail.com>
2025-12-14 13:20:14 +0100

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/lab10sarahahmed/simpleBlog/SimpleBlogApplication.java
@@ -0,0 +1,13 @@
+package com.lab10sarahahmed.simpleBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab10sarahahmed/simpleBlog/SimpleBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab10sarahahmed.simpleBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
